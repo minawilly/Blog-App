1c0205b6b823580ab1296e5564f819fc4d06b059
minawilly <aminahwilliamsade@gmail.com>
2022-06-18 21:53:21 +0100

    initial commit

---
+++ I4G017455MVE/blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+
+class Post(models.Model):
+    Title = models.CharField(max_length=200)
+
+    Text = models.TextField
+
+    Author = models.ForeignKey(get_user_model(), models.CASCADE)
+
+    Created_date = models.DateTimeField
+
+    Published_date = models.DateTimeField
